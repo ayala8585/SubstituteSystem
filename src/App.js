14777c5c085a0684a7a55f5eca780bef9605a776
הינמן אילה <324218585@mbydomain>
2021-05-19 19:07:43 +0300

    finish wed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Login from './components/login/login'
+import SignUp from './components/signUp/signUp'
+import SignUpPrincipal from './components/signUp/signUpPrincipal'
+import SignUpTeacher from './components/signUp/signUpTeacher'
+import SignUpSubstitute from './components/signUp/signUpSubstitute'
 import './App.css';
 import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
+import signUp from './components/signUp/signUp';
 
 function App() {
   return (
@@ -10,6 +15,24 @@ function App() {
         <Switch>
           <Route path='/login'>
             <Login/>
+            <SignUp/>
+          </Route>
+
+          <Route path='/signUpPrincipal'>
+            <SignUpPrincipal/>
+          </Route>
+
+          <Route path='/signUpTeacher'>
+            <SignUpTeacher/>
+          </Route>
+
+          <Route path='/signUpSubstitute'>
+            <SignUpSubstitute/>
+          </Route>
+
+          <Route path='/'>
+            <Login/>
+            <SignUp/>
           </Route>
         </Switch>
      </Router>
