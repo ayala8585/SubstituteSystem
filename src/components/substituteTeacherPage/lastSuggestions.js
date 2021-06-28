44c745217d9e633e8ad2e143035f530607c367eb
מלכה גרוס <212044127@mbydomain>
2021-06-28 16:06:29 +0300

    תיקון קומפוננטה פרטי מ"מ

---
+++ src/components/substituteTeacherPage/lastSuggestions.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+LastSuggestions=()=>{
+    [suggestionsArr,setSuggestionsArr]=useState([]);
+    useEffect=()=>{
+        setSuggestions=()=>{
+            
+        }
+    }
+}
+
+export default LastSuggestions;
