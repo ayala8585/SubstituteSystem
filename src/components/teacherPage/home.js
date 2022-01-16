d4e5b8742f8e0fccd7b8e17559195a74965e428c
malka-farkash <m81653@gmail.com>
2022-01-16 14:00:17 +0200

    warnings

--- src/components/teacherPage/home.js
+++ src/components/teacherPage/home.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import SubtractionHistory from './subtractionHistory'
-import SetDetails from './setDetails'
-import SubtractionReport from './subtractionReport'
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import HistoryIcon from '@material-ui/icons/History';
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
-
+import React from "react";
+import SubtractionHistory from "./subtractionHistory";
+import SetDetails from "./setDetails";
+import SubtractionReport from "./subtractionReport";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { CalendarToday, History, PersonPin } from "@material-ui/icons";
+import { Box, Typography, Tab, Tabs, AppBar } from "@material-ui/core";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -45,14 +36,14 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `scrollable-prevent-tab-${index}`,
-    'aria-controls': `scrollable-prevent-tabpanel-${index}`,
+    "aria-controls": `scrollable-prevent-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    width: '100%',
+    width: "100%",
     backgroundColor: theme.palette.background.paper,
   },
 }));
@@ -75,21 +66,24 @@ export default function ScrollableTabsButtonPrevent() {
           scrollButtons="off"
           aria-label="scrollable prevent tabs example"
         >
-          <Tab icon={<CalendarTodayIcon />} aria-label="phone" {...a11yProps(0)} />
-          <Tab icon={<HistoryIcon />} aria-label="favorite" {...a11yProps(1)} />
-          <Tab icon={<PersonPinIcon />} aria-label="person" {...a11yProps(2)} />
+          <Tab
+            icon={<CalendarToday />}
+            aria-label="phone"
+            {...a11yProps(0)}
+          />
+          <Tab icon={<History />} aria-label="favorite" {...a11yProps(1)} />
+          <Tab icon={<PersonPin />} aria-label="person" {...a11yProps(2)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <SubtractionReport/>
+        <SubtractionReport />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <SubtractionHistory/> 
+        <SubtractionHistory />
       </TabPanel>
       <TabPanel value={value} index={2}>
-        <SetDetails/>
+        <SetDetails />
       </TabPanel>
     </div>
   );
 }
-
--- src/components/teacherPage/subtractionReport.js
+++ src/components/teacherPage/subtractionReport.js
@@ -1,81 +1,58 @@
-import React, { useState, useEffect } from 'react'
-// import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import FolderIcon from '@material-ui/icons/Folder';
-import DeleteIcon from '@material-ui/icons/Delete';
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
-import SelectHour from './selectHour'
-import { useHistory } from "react-router-dom"
-import '../login/login.css'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Autocomplete, {
+  createFilterOptions,
+} from "@material-ui/lab/Autocomplete";
+import {
+  Grid,
+  List,
+  Avatar,
+  Button,
+  ListItem,
+  TextField,
+  Typography,
+  IconButton,
+  ListItemText,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+} from "@material-ui/core";
+import { CalendarToday, Delete } from "@material-ui/icons";
 
-
+import "../login/login.css";
+import SelectHour from "./selectHour";
 
 const SubtractionReport = () => {
-
-  const [identity, setIdentity] =useState(0);
-  const institutionsList = [//select the teachers institutions list from db
-    { title: 'The Kid', year: 1921 },
-    { title: 'Inglourious Basterds', year: 2009 },
-    { title: 'Snatch', year: 2000 },
-    { title: '3 Idiots', year: 2009 },
-    { title: 'Monty Python and the Holy Grail', year: 1975 },
+  const [identity, setIdentity] = useState(0);
+  const institutionsList = [
+    //select the teachers institutions list from db
+    { title: "The Kid", year: 1921 },
+    { title: "Inglourious Basterds", year: 2009 },
+    { title: "Snatch", year: 2000 },
+    { title: "3 Idiots", year: 2009 },
+    { title: "Monty Python and the Holy Grail", year: 1975 },
   ];
 
-  // const useStyles = makeStyles((theme) => ({
-  //     root: {
-  //       display: 'flex',
-  //       flexWrap: 'wrap',
-  //     },
-  //     textField: {
-  //       marginLeft: theme.spacing(1),
-  //       marginRight: theme.spacing(1),
-  //       width: '25ch',
-  //     },
-  //   }));
-
-  //   const classes = useStyles();
-
-  // const classes = useStyles();
-
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
-
+  const [dense, setDense] = useState(false);
 
   const filterOptions = createFilterOptions({
-    matchFrom: 'start',
+    matchFrom: "start",
     stringify: (option) => option.title,
   });
 
   const deleteHour = (id) => {
     const newList = subtractionHoursList.filter((item) => item.id !== id);
     setSubtractionHoursList(newList);
-  }
+  };
 
   //create useState and accept the value from SelectHour component
   const [subtractionHoursList, setSubtractionHoursList] = useState([]);
 
   let history = useHistory();
 
-  const addHoure = () => {
-    console.log("kkk")
-    history.push('/selectHour');
-  }
+  const addHour = () => {
+    console.log("kkk");
+    history.push("/selectHour");
+  };
 
   const [showSelectHour, setShowSelectHour] = useState(false);
 
@@ -96,25 +73,24 @@ const SubtractionReport = () => {
     // setSubtractionDate(date);
     // setStartOfClass(start);
     // setEndOfClass(end);
-    setIdentity(identity+1);
+    setIdentity(identity + 1);
     let hour = {
       id: identity,
       date: date,
       grade: grade,
       start: start,
       end: end,
-      profession: profession
-    }
-    setSubtractionHoursList([...subtractionHoursList, hour])
-  }
-
+      profession: profession,
+    };
+    setSubtractionHoursList([...subtractionHoursList, hour]);
+  };
 
   const listItems = () => {
-    return (subtractionHoursList.map(hour =>
+    return subtractionHoursList.map((hour) => (
       <ListItem key={hour.id}>
         <ListItemAvatar>
           <Avatar>
-            <CalendarTodayIcon />
+            <CalendarToday />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
@@ -122,22 +98,25 @@ const SubtractionReport = () => {
           secondary={hour.grade}
         />
         <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="delete" onClick={() => deleteHour(hour.id)}>
-            <DeleteIcon />
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={() => deleteHour(hour.id)}
+          >
+            <Delete />
           </IconButton>
         </ListItemSecondaryAction>
       </ListItem>
-    ))
+    ));
   };
 
   const selectDate = () => {
-    return <SelectHour />
-  }
-
+    return <SelectHour />;
+  };
 
   return (
     <div>
-      <div >
+      <div>
         <TextField
           id="outlined-full-width"
           label="פרוט הבקשה"
@@ -158,52 +137,53 @@ const SubtractionReport = () => {
         getOptionLabel={(option) => option.title}
         filterOptions={filterOptions}
         style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="מוסד לימודים" variant="outlined" />}
+        renderInput={(params) => (
+          <TextField {...params} label="מוסד לימודים" variant="outlined" />
+        )}
       />
-
 
       <div>
         <Grid item xs={12} md={6}>
-          <Typography variant="h6" >
-            שעות חיסור
-          </Typography>
+          <Typography variant="h6">שעות חיסור</Typography>
           <div>
-            <List dense={dense}>
-              {listItems()}
-            </List>
+            <List dense={dense}>{listItems()}</List>
           </div>
         </Grid>
       </div>
 
-
-      <Button variant="contained" color="primary" disableElevation onClick={() => setShowSelectHour(true)}>
+      <Button
+        variant="contained"
+        color="primary"
+        disableElevation
+        onClick={() => setShowSelectHour(true)}
+      >
         הוספת שעת חיסור
-          </Button>
+      </Button>
       <br />
-      {showSelectHour ? <SelectHour addToList={addToList}  setShowSelectHour={setShowSelectHour}/> : null}
+      {showSelectHour ? (
+        <SelectHour
+          addToList={addToList}
+          setShowSelectHour={setShowSelectHour}
+        />
+      ) : null}
       {/* setSubtractionDate={setSubtractionDate} setStartOfClass={setStartOfClass} setEndOfClass={setEndOfClass} */}
       <br />
-      {(subtractionHoursList.length>0 && !showSelectHour) ?<Button variant="contained" color="primary" disableElevation onClick={() =>{}}>
-        שליחת הבקשה
-          </Button>:null}
-          {/* post */}
-        </div>
+      {subtractionHoursList.length > 0 && !showSelectHour ? (
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={() => {}}
+        >
+          שליחת הבקשה
+        </Button>
+      ) : null}
+      {/* post */}
+    </div>
   );
-
-}
+};
 
 export default SubtractionReport;
-
-
-
-
-
-
-
-
-
-
-
 
 // const useStyles = makeStyles((theme) => ({
 //   root: {
@@ -217,5 +197,3 @@ export default SubtractionReport;
 //     margin: theme.spacing(4, 0, 2),
 //   },
 // }));
-
-
