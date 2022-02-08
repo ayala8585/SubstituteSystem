62e61d80385a82ccca09aa7e1d8bb18bf51b3808
malka-farkash <m81653@gmail.com>
2022-02-08 17:07:42 +0200

    login form

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,100 +1,86 @@
-import React, { useState, useEffect } from "react";
-import { Container } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-import ValidateTextField from "../common/validateTextField";
-import "./login.css";
-import Button from "@material-ui/core/Button";
-import jwt_decode from "jwt-decode";
+import React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { Button, TextField } from "@material-ui/core";
+// import jwt_decode from "jwt-decode";
+
+import "../signUp/signUpTeacher.css";
+
+const validationSchema = yup.object({
+  id: yup.string().required("שדה חובה"),
+  password: yup.string().required("שדה חובה"),
+});
 
 const Login = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      "& .MuiTextField-root": {
-        margin: theme.spacing(1),
-        width: 200,
-      },
-    },
-  }));
+  const onSubmit = async (values) => {
+    console.log("submit");
+    try {
+      const response = await fetch(
+        `http://localhost:5000/login?id=${values.id}&password=${values.password}`,
+        {
+          method: "GET",
+          // body: JSON.stringify(values),
+        }
+      ).then((res) => res.json());
+      console.log("after");
+      console.log({response})
 
-  const getUser = () => {
-    fetch(
-      `http://localhost:3000/login?userName=${userName}&password=${password}`
-    )
-      .then((res) => res.json())
-      .then((token) => {
-        const decoded = jwt_decode(token);
-        console.log(decoded);
-      })
-      .catch((err) => {
-        console.log("error", err);
-      });
+      const { token, contact } = response
+
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.log({ error });
+    }
+    // .then((res) => res.json())
+    // .then((token) => {
+    //   const decoded = jwt_decode(token);
+    //   console.log(decoded);
+    // })
+    // .catch((err) => {
+    //   console.log("error", err);
+    // });
   };
 
+  const { handleSubmit, getFieldProps, touched, errors, values } = useFormik({
+    initialValues: {
+      id: "",
+      password: "",
+    },
+    onSubmit,
+    validateOnChange: false,
+    validationSchema,
+  });
+
   return (
-    <div className="login">
+    <form onSubmit={handleSubmit} className="form">
       <h3>התחברות</h3>
-      <div className="div1">
-        <ValidateTextField
-          label="שם משתמש"
-          text={userName}
-          onChange={(value) => setUserName(value)}
-          validate={() => {
-            return userName === "";
-          }}
-          errorMessage={"שדה חובה"}
-        />
-        <ValidateTextField
-          label="סיסמה"
-          text={password}
-          onChange={(value) => setPassword(value)}
-          validate={() => {
-            return password === "";
-          }}
-          errorMessage={"שדה חובה"}
-        />
-      </div>
+      <TextField
+        label="מספר זהות"
+        required
+        variant="outlined"
+        error={touched?.id && !!errors?.id}
+        helperText={touched?.id && errors?.id}
+        {...getFieldProps("id")}
+      />
+      <TextField
+        label="סיסמה"
+        required
+        variant="outlined"
+        error={touched?.password && !!errors?.password}
+        helperText={touched?.password && errors?.password}
+        {...getFieldProps("password")}
+      />
       <Button
+        type="submit"
         variant="contained"
         color="primary"
-        disableElevation
-        onClick={getUser}
-        disabled={!userName || !password}
+        disabled={!values.id || !values.password}
       >
         כניסה
       </Button>
-    </div>
+    </form>
   );
 };
 
 export default Login;
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& .MuiTextField-root': {
-//       margin: theme.spacing(1),
-//       width: 200,
-//     },
-//   },
-// }));
-
-// export default function ValidationTextFields() {
-//   const classes = useStyles();
-//   return (
-//     <form className={classes.root} noValidate autoComplete="off">
-//       <div>
-//         <TextField
-//           error
-//           id="outlined-error-helper-text"
-//           label="name"
-//           defaultValue="Hello World"
-//           helperText="Incorrect entry."
-//           variant="outlined"
-//         />
-//       </div>
-//     </form>
-//   );
-// }
